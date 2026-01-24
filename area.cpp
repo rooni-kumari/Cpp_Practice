a4ed16de746cd6029579d8780bae27e9d507f8ff
rooni-kumari <kumarirooni44@gmail.com>
2026-01-24 19:07:30 +0530

    area

---
+++ area.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r;
+	float area;
+	cout<<"enter radius of square"<<endl;
+	cin>>r;
+	area=r*r;
+	cout<<"your area is:"<<area<<endl;
+	return 0;
+	
+}
