19f2e9ceecee332d91029c47e3e557416947ce0b
rooni-kumari <kumarirooni44@gmail.com>
2026-01-30 16:33:39 +0530

    12345 pattern

---
+++ 123_patt.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m=6,n=6;
+    
+    for(int i=1;i<=m;i++)
+    {
+        int b=1;
+        for(int j=1;j<=i;j++)
+        {
+            cout<<b;
+            b=b+1;
+
+        }
+        cout<<endl;
+        
+
+    }
+    return 0;
+}
