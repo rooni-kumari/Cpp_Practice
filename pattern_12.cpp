7aebeb8b8051dfd303395d3c9789d779c0c3ff35
rooni-kumari <kumarirooni44@gmail.com>
2026-01-30 13:37:43 +0530

    pattern of 12345

---
+++ pattern_12.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[5][5];
+    for(int i=1;i<6;i++)
+    {
+        for(int j=1;j<6;j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
