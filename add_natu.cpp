f9345a2e6592d52f032572c4688746913d1756d0
rooni-kumari <kumarirooni44@gmail.com>
2026-01-31 21:23:51 +0530

    add of natural

---
+++ add_natu.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int count=0,num=1;
+
+for(int i=0;i<10;i++)
+
+{
+
+count=count+num;
+
+num++;
+
+}
+cout<<count<<endl;
+return 0;
+
+}
