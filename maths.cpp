0d85bb2f7d5e54e62dee387c2c20a980a8bc5848
rooni-kumari <kumarirooni44@gmail.com>
2026-02-03 13:11:47 +0530

    star pyramid

---
+++ maths.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b,c;
+
+    cout << "Enter three numbers: ";
+    cin >> a >> b>>c;
+
+    int total=a * - b / c;
+
+    cout << "Greatest number is: " << total;
+
+    return 0;
+}
